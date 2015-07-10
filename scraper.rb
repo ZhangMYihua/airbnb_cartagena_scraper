c713899836bc216a2482d5eaad5bf4fcf7ad1d70
zhangmyihua <michaelzhang2@msn.com>
2015-07-11 03:28:49 +0800

    stripped white space as well as added csv file from code

--- scraper.rb
+++ scraper.rb
@@ -14,7 +14,7 @@ price = []
 details = []
 
 page.css('h3.h5.listing-name.text-truncate').each do |element|
-  name << element.text
+  name << element.text.strip
 end
 
 page.css('span.h3.price-amount').each do |cost|
@@ -22,7 +22,7 @@ page.css('span.h3.price-amount').each do |cost|
 end
 
 page.css('div.text-muted.listing-location.text-truncate').each do |detail|
-  details << detail
+  details << detail.text.strip
 end
 
 # Write data to csv file
@@ -33,3 +33,5 @@ CSV.open("airbnb_listing.csv", "w") do |file|
     file << [name[i], price[i], details[i]]
   end
 end 
+
+puts page.css('div.text-muted.listing-location.text-truncate')
