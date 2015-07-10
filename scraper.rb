6fca8d1178afc54f85325367872a66f5ec39ac37
zhangmyihua <michaelzhang2@msn.com>
2015-07-11 04:58:35 +0800

    moved arrays out of loop for max_page, url is set to each page number in max_page variable

--- scraper.rb
+++ scraper.rb
@@ -8,45 +8,54 @@ url = "https://www.airbnb.ca/s/Cartagena-~-Bolivar--Colombia"
 # Parse the page with nokogiri
 page = Nokogiri::HTML(open(url))
 
+# store page numbers in an array variable, convert all to integers first
 page_numbers = []
 page.css("div.pagination ul li a[target]").each do |element|
-  page_numbers << element
+  page_numbers << element.text.to_i
 end
 
-puts page_numbers
+max_page = page_numbers.max
 
-# Store data in arrays
-name = []
-price = []
-details = []
+# Initialize empty arrays
+  name = []
+  price = []
+  details = []
 
-page.css('h3.h5.listing-name.text-truncate').each do |element|
-  name << element.text.strip
-end
+# Loop once for every page of search results
+max_page.times do |n|
 
-page.css('span.h3.price-amount').each do |cost|
-  price << cost.text
-end
+  url = "https://www.airbnb.ca/s/Cartagena-~-Bolivar--Colombia?page=#{n+1}"
+  page = Nokogiri::HTML(open(url))
 
-page.css('div.text-muted.listing-location.text-truncate').each do |detail|
-  details << detail.text.gsub(/\s/, ' ').squeeze(' ').strip.split(/ · /)
-end
 
-details.length.times do |n|
-  if details[n][1]
-    details[n][1] = details[n][1].gsub(/· /, '').strip
-  else
-    details[n] << "No reviews"
+  page.css('h3.h5.listing-name.text-truncate').each do |element|
+    name << element.text.strip
   end
+
+  page.css('span.h3.price-amount').each do |cost|
+    price << cost.text
+  end
+
+  page.css('div.text-muted.listing-location.text-truncate').each do |detail|
+    details << detail.text.gsub(/\s/, ' ').squeeze(' ').strip.split(/ · /)
+  end
+
+  details.length.times do |n|
+    if details[n][1]
+      details[n][1] = details[n][1].gsub(/· /, '').strip
+    else
+      details[n] << "No reviews"
+    end
+  end
+
 end
 
-# puts page.css('div.text-muted.listing-location.text-truncate')
 
-# # Write data to csv file
-# CSV.open("airbnb_listing.csv", "w") do |file|
-#   file << ["Listing Name", "Price", "Room", "Reviews"]
+# Write data to csv file
+CSV.open("airbnb_listing.csv", "w") do |file|
+  file << ["Listing Name", "Price", "Room", "Reviews"]
 
-#   name.length.times do |i|
-#     file << [name[i], price[i], details[i][0], details[i][1]]
-#   end
-# end 
+  name.length.times do |i|
+    file << [name[i], price[i], details[i][0], details[i][1]]
+  end
+end 
