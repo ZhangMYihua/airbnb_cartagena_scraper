b1e6b607c2f0fec32c12f46125d08fe0f6fa608a
zhangmyihua <michaelzhang2@msn.com>
2015-07-11 03:17:12 +0800

    data parsed gets stored in arrays to be displayed in csv

--- scraper.rb
+++ scraper.rb
@@ -8,3 +8,15 @@ url = "https://www.airbnb.ca/s/Cartagena-~-Bolivar--Colombia"
 #parse the page with nokogiri
 page = Nokogiri::HTML(open(url))
 
+# store data in arrays
+name = []
+price = []
+
+page.css('h3.h5.listing-name.text-truncate').each do |element|
+  name << element.text
+end
+
+page.css('span.h3.price-amount').each do |cost|
+  price << cost.text
+end
+
